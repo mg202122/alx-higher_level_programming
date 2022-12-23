0070d806bcc3b5191f51cac8c3d3270341238520
mg202122 <otifarms@gmail.com>
2022-12-23 06:04:59 -0800

    Deadline endeded task

---
+++ READ.md
@@ -0,0 +1,5 @@
+Date : 23/12/2022
+
+Author: Derrick Oti
+
+0x06. Python - Classes and Objects
---
+++ lines.py
@@ -0,0 +1,45 @@
+a = 12
+if a > 2:
+    if a % 2 == 0:
+        print("Holberton")
+    else:
+        print("C is fun")
+else:
+    print("School")
+
+if True:
+    print("Holberton")
+else:
+    print("School")
+
+for i in range(2, 4):
+    print(i, end=" ")
+
+if 12 == 48/4 and False:
+    print("Holberton")
+else:
+    print("School")
+
+a = 12
+if a < 2:
+    print("Holberton")
+elif a % 2 == 0:
+    print("C is fun")
+else:
+    print("School")
+
+if 12 == 48/3 or 12 == 12:
+    print("Holberton")
+else:
+    print("School")
+
+if 12 == 48/3 or 12 == 12:
+    print("Holberton")
+else:
+    print("School")
+
+for i in range(2, 10, 2):
+    print(i, end="\n ")
+
+for i in range(4):
+    print(i, end="\n ")
